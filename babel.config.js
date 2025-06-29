2e1d79419e9cc5dd962f8ed0c726095745fd0f57
Purv Virpariya <73741156+purv07@users.noreply.github.com>
2025-06-29 17:33:51 +0530

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-transform-runtime',
-      'react-native-reanimated/plugin',
+      '@babel/plugin-transform-modules-commonjs',
+      'react-native-reanimated/plugin' // keep this last
     ],
   };
 };
